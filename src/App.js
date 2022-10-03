21bd0268feed7fdb8aa23c22e4c21415f1ffd079
ardaalkan <ardaaa.alk@gmail.com>
2022-10-03 15:35:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useSWR, { SWRConfig } from "swr";
 
-function App() {
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SWRConfig value={{ revalidateOnFocus: false, fetcher }}>
+      <Character />
+    </SWRConfig>
   );
 }
 
-export default App;
+function Character() {
+  const url = "https://www.breakingbadapi.com/api/characters/";
+  //we can pass path , url or graphql query.
+  const { data, error } = useSWR(url);
+
+  if (error) {
+    return <div>Error..</div>;
+  }
+
+  if (!data) {
+    return <div>Loading..</div>;
+  }
+
+  return (
+    <DisplayCharacter
+      character={data}
+      names={[...new Set(data.map((value) => value.name))]}
+    />
+  );
+}
+
+function DisplayCharacter({ character, names }) {
+  const [filterName, setFilterName] = useState(null);
+  const filteredCharacters = filterName
+    ? character.filter((ch) => ch.name === filterName)
+    : character;
+
+  return (
+    <>
+      {names.map((name) => (
+        <button onClick={() => setFilterName(name)} key={name}>
+          {name}
+        </button>
+      ))}
+      {filterName && (
+        <button
+          onClick={() => {
+            setFilterName(null);
+          }}
+        >
+          reset
+        </button>
+      )}
+      <pre>{JSON.stringify(filteredCharacters, null, 5)}</pre>
+    </>
+  );
+}
+
+// It will show us the cached version of data if it has there it
+// and then it the background we'll go and fetch the new version
+// and it will replace that once it gets it.
+
+// detects transitions between tabs
